f77b70b7ad0d5214b5766d9a2374fda58a9389b2
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-02 11:33:53 +0100

    12-bracin_the_elements.py

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-import numpy as np
+"""12. Bracing The Elements """
+
 
 def np_elementwise(mat1, mat2):
     "function that performs element-wise addition, subtraction, multiplication, and division"
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1,mat2)
-    div = np.divide(mat1, mat2)
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
     return add, sub, mul, div
