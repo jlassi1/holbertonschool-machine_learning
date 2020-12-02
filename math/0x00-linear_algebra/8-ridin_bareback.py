207ecfc646b1f18fc6e07a69d9616391ede20573
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-02 11:18:32 +0100

    8-ridin_bareback.py

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -6,10 +6,10 @@ def mat_mul(mat1, mat2):
     "function that performs matrix multiplication"
     if len(mat1[0]) != len(mat2):
         return None
-    else:
-        mat = [[0  for j in range(len(mat2[0]))] for i in range(len(mat1))]
-        for i in range(len(mat1)):
-            for j in range(len(mat2[0])):
-                for k in range(len(mat2)):
-                    mat[i][j] += mat1[i][k] * mat2[k][j]
-        return mat
+
+    mat = [[0  for j in range(len(mat2[0]))] for i in range(len(mat1))]
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                mat[i][j] += mat1[i][k] * mat2[k][j]
+    return mat
