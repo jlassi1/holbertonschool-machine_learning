809f9bec8fe450731ff03fc1c29628db1bf3a8e3
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-02 11:54:09 +0100

    13-cats_got_your_tongue.py

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """  13. Cat's Got Your Tongue """
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    "function that concatenates two matrices along a specific axis"
-    import numpy as np
+    """ function that concatenates two matrices along a specific axis """
     return np.concatenate((mat1, mat2), axis=axis)
