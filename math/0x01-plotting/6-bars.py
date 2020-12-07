0a00926a2bbd8a93917bf2fc5fe21832fe2882d6
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-07 22:21:19 +0100

    6

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -5,29 +5,15 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 fruit = np.random.randint(0, 20, (4, 3))
 
-
 width = 0.5
 colors = ['r', 'yellow', '#ff8000', '#ffe5b4']
+labels= ['apples', 'bananas', 'oranges', 'peaches']
 names = ['Farrah', 'Fred', 'Felicia']
 bottom = np.zeros(3)
 
-
-for elem, color in zip(fruit, colors):
-    if color == 'r':
-        plt.bar(names, elem, width, bottom=bottom, color=color, label='apples')
-        bottom += elem
-    elif color == "yellow":
-        plt.bar(names, elem, width, bottom=bottom,
-                color=color, label='bananas')
-        bottom += elem
-    elif color == "#ff8000":
-        plt.bar(names, elem, width, bottom=bottom,
-                color=color, label='oranges')
-        bottom += elem
-    else:
-        plt.bar(names, elem, width, bottom=bottom,
-                color=color, label='peaches')
-        bottom += elem
+for elem, color, label in zip(fruit, colors, labels):
+    plt.bar(names, elem, width, bottom=bottom, color=color, label=label)
+    bottom += elem
 
 plt.ylim(0, 80)
 plt.ylabel('Quantity of Fruit')
