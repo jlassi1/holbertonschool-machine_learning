457b83ea6d0e0bf691af98b81736a126618b1e52
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-08 00:04:30 +0100

    100-gradient.py

--- math/0x01-plotting/100-gradient.py
+++ math/0x01-plotting/100-gradient.py
@@ -8,7 +8,6 @@ x = np.random.randn(2000) * 10
 y = np.random.randn(2000) * 10
 z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
-# your code here
 
 plt.scatter(x, y, c=z)
 plt.colorbar(label="elevation (m)")
