6b1e16a281e62f333049874607e5de1f1821010e
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-08 00:03:21 +0100

    101-pca.py

--- math/0x01-plotting/101-pca.py
+++ math/0x01-plotting/101-pca.py
@@ -6,10 +6,25 @@ import numpy as np
 lib = np.load("pca.npz")
 data = lib["data"]
 labels = lib["labels"]
-
+# print(labels)
+# print(data)
 data_means = np.mean(data, axis=0)
 norm_data = data - data_means
 _, _, Vh = np.linalg.svd(norm_data)
-pca_data = np.matmul(norm_data, Vh[:3].T)
 
-# your code here
+pca_data = np.matmul(norm_data, Vh[:3].T)
+# print(pca_data)
+
+
+fig = plt.figure()
+ax = Axes3D(fig)
+
+ax.scatter(pca_data[:, 0], pca_data[:, 1], pca_data[:, 2], c=labels,
+           cmap='plasma', marker='o')
+plt.xlabel('U1')
+plt.ylabel('U2')
+ax.set_zlabel('U3')
+
+plt.title("PCA of Iris Dataset")
+
+plt.show()
