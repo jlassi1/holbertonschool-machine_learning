c27d5215611f614dd488ab7f9a55bc398286119e
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-07 19:54:15 +0100

    4-frequency.py

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-
+plt.xticks(range(0, 101, 10))
 plt.hist(student_grades, bins=[i for i in range(0, 101, 10)], edgecolor='k')
 plt.xlim(0, 100)
 plt.ylim(0, 30)
