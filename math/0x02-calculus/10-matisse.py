78ae5e5f4312237957bdecf9ce6eda5164b6213d
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-09 22:27:29 +0100

    10-matisse.py

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -5,10 +5,9 @@
 def poly_derivative(poly):
     """function that calculates the derivative of a polynomial"""
     if not isinstance(poly, list) or not all(
-            isinstance(x, (
-                    int, float)) for x in poly) or poly is None or poly == []:
+            isinstance(x, (int, float)) for x in poly) or poly is (None or []):
         return None
-    if len(poly) == 1:
+    if len(poly) <= 1:
         return[0]
     new_poly = [coeff * idx for idx, coeff in enumerate(poly[1:], 1)]
     if all(x == 0 for x in new_poly):
