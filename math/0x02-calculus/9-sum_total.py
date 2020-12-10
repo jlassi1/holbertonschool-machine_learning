f1bc7f0fbc4ae4e91091fb927a5e21952a540356
jlassi1 <57707783+jlassi1@users.noreply.github.com>
2020-12-10 21:20:12 +0100

    Update 9-sum_total.py

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,4 +6,4 @@ def summation_i_squared(n):
     """ calculate given sum """
     if type(n) != int or n < 1:
         return None
-    return (n * (n + 1) * (2 * n + 1)) / 6
+    return (n * (n + 1) * (2 * n + 1)) // 6
