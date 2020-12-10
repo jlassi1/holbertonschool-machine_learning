ee62fe70dad400af5fbc2d506d9cdd9c71a152f8
jlassi1 <57707783+jlassi1@users.noreply.github.com>
2020-12-10 21:19:18 +0100

    Update 9-sum_total.py

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python 3
-"""Sum squared"""
+""" Sum squared """
 
 
 def summation_i_squared(n):
-    """calculate given sum"""
-    if type(n) != int and n < 1:
+    """ calculate given sum """
+    if type(n) != int or n < 1:
         return None
-    return int(n * (n + 1) * (2 * n + 1) / 6)
+    return (n * (n + 1) * (2 * n + 1)) / 6
