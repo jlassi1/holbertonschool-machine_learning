4630e79a0e8c7a7b989cbf5df093119e77e457ca
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-10 21:04:08 +0100

    .\9-sum_total.py

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" summation function """
+""" summation function"""
 
 
 def summation_i_squared(n):
-    """ function that calculates [\\sum_{i=1}^{n} i^2] """
+    """ function that calculates [\\sum_{i=1}^{n} i^2]"""
     if not isinstance(n, (int, float)) or n < 1:
         return None
     return (n * (n + 1) * (2 * n + 1)) // 6
