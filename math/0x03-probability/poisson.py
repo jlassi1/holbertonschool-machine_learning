46253bb163571f55e5a7d79ea2b424c15dab0096
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-15 13:41:24 +0100

    poisson.py

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """ Poisson Distribution"""
 
+
 class Poisson:
     """Poisson Class """
+    e = 2.7182818285
     def __init__(self, data=None, lambtha=1.):
         """ intitializetion of poisson class """
+        self.data = data
         if data == None:
             if lambtha < 0:
                 raise ValueError("lambtha must be a positive value")
@@ -18,3 +21,14 @@ class Poisson:
             else:
                 self.lambtha = sum(data) / len(data)
 
+
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of “successes”"""
+        if not isinstance(k, int):
+            self.k = int(k)
+        if k < 0:
+            return 0
+        factorial = 1
+        for i in range(1,k + 1):
+            factorial = factorial*i
+        return ((self.e**(-self.lambtha) *(self.lambtha**k)) / factorial)
