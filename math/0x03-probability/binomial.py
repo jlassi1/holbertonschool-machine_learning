ae8312bc4b0bf8ebcaa5c8eb3fca76c7d55059fc
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-16 11:30:11 +0100

    ba

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -8,4 +8,26 @@ class Binomial:
     """Binomail Class """
     def __init__(self, data=None, n=1, p=0.5):
         """intitialization of class Binomial"""
-        
+        self.data = data
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            else:
+                self.n = int(n)
+            if p < 0 or p > 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            else:
+                self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            mean = float(sum(data) / len(data))
+            SD = 0
+            for x in data:
+                SD += float((x - mean) ** 2)
+            sqrtstddev = float(SD / (len(data)))
+            self.p = 1 - sqrtstddev / mean
+            self.n = round(mean / self.p)
+            self.p = mean / self.n
