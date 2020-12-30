3ec1d59a3382b4a268bdfa1a987ec53fd9fc73cc
jlassi1 <khawlajlassi1990@gmail.com>
2020-12-30 03:41:04 -0800

    11-neural_network.py

--- supervised_learning/0x01-classification/11-neural_network.py
+++ supervised_learning/0x01-classification/11-neural_network.py
@@ -4,10 +4,11 @@ import numpy as np
 
 
 class NeuralNetwork:
-    """defines a neural network with one hidden layer performing binary classification"""
+    """defines a neural network with one
+    hidden layer performing binary classification"""
 
     def __init__(self, nx, nodes):
-        """Class Initialization """
+        """Class Initialization"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         elif nx < 1:
@@ -16,11 +17,12 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         elif nodes < 1:
             raise ValueError("nodes must be a positive integer")
+        self.nodes = nodes
         self.nx = nx
-        self.__W1 = np.random.randn(3, self.nx)
+        self.__W1 = np.random.randn(self.nodes, self.nx)
         self.__A1 = 0
-        self.__b1 = np.zeros((len(self.W1), 1))
-        self.__W2 = np.random.randn(1, len(self.W1))
+        self.__b1 = np.zeros((self.nodes, 1))
+        self.__W2 = np.random.randn(1, self.nodes)
         self.__A2 = 0
         self.__b2 = 0
 
