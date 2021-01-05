feae988ef7f3c6d786e1a52c2b03f937bca177b1
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-05 08:56:24 +0100

    13-neural_network.py

--- supervised_learning/0x01-classification/13-neural_network.py
+++ supervised_learning/0x01-classification/13-neural_network.py
@@ -86,16 +86,20 @@ class NeuralNetwork:
 
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
         """Calculates one pass of gradient descent on the neural network """
-        nx, m = Y.shape
+        m = X.shape[1]
         dz2 = A2 - Y
-        dw2 = (1/m) * np.matmul(dz2, A1.T)
-        db2 = (1/m) * np.sum(dz2, axis=1, keepdims=True)
+        dz1 = np.dot(self.__W2.T, dz2) * self.sigmoid_derivative(A1)
 
-        dz1 = np.matmul(self.__W2.T, dz2) * (A1 * (1 - A1))
-        dw1 = (1/m) * np.matmul(dz1, X.T)
-        db1 = (1/m) * np.sum(dz1, axis=1, keepdims=True)
-        self.__W2 -= (dw2 * alpha)
-        self.__b2 -= (db2 * alpha)
-        self.__W1 -= (dw1 * alpha)
-        self.__b1 -= (db1 * alpha)
+        dW1 = np.dot(dz1, X.T) / m
+        self.__W1 = self.__W1 - alpha * dW1
+
+        dW2 = np.dot(dz2, A1.T) / m
+        self.__W2 = self.__W2 - alpha * dW2
+
+        db1 = np.sum(dz1, axis=1, keepdims=True) / m
+        self.__b1 = self.__b1 - alpha * db1
+
+        db2 = np.sum(dz2, axis=1, keepdims=True) / m
+        self.__b2 = self.__b2 - alpha * db2
+
         return self.__W1, self.__b1, self.__W2, self.__b2
