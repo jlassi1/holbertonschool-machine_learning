a082c123a92d8381d9ff7b054a97ec3330a61dc7
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-07 18:49:26 +0100

    25-one_hot_decode.py

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -6,6 +6,8 @@ import numpy as np
 def one_hot_decode(one_hot):
     """function that converts a one-hot matrix into a vector of labels"""
     try:
-        return np.argmax(one_hot.T, axis=1)
+        if len(one_hot.shape) != 2:
+            return None
+        return np.argmax(one_hot, axis=0)
     except Exception:
         return None
