ec029b0d96eed8d4cd0b52b631a4752c7487d512
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-12 12:52:47 +0100

    5-momentum.py

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -6,6 +6,6 @@ import numpy as np
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """function that updates a variable using
     the gradient descent with momentum optimization algorithm"""
-    grad = beta1*v + (1-beta1)*grad
+    v = beta1*v + (1-beta1)*grad
     var = var - alpha*v
-    return var, grad
+    return var, v
