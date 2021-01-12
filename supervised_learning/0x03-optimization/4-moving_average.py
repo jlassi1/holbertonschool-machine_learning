c5429b74e5d66a78f35e6562fa1655f1d66082c7
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-12 11:49:41 +0100

    list

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -6,10 +6,10 @@ import numpy as np
 def moving_average(data, beta):
     """function  that calculates the weighted moving average of a data set"""
     n = len(data)
-    zs = np.zeros(n)
+    zs = []
     z = 0
     for i in range(n):
         z = beta*z + (1 - beta)*data[i]
         zc = z/(1 - beta**(i+1))
-        zs[i] = zc
+        zs.append(zc)
     return zs
