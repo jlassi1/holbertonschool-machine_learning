ea799a3860ce7fb54097de159115c64e2bcc368e
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-18 16:14:33 +0100

    3-specificity.py

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 """3. Specificity"""
+import numpy as np
+
+
+def specificity(confusion):
+    """function that calculates the specificity for
+    each class in a confusion matrix"""
+    """ specificity = TN/(TN+FP)"""
+
+    TP = np.diag(confusion)
+    FP = np.sum(confusion, axis=0) - TP
+    FN = np.sum(confusion, axis=1) - TP
+    TN = confusion.sum() - (TP+FP+FN)
+
+    return TN / (TN + FP)
