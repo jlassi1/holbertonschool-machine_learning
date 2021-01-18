0231df18cbe6442f3e7e0e0184f0fba0eeba6f14
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-18 16:35:30 +0100

    4-f1_score.py

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 """4. F1 score """
+
+import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """function that calculates the F1 score of a confusion matrix:"""
+    """ specificity = TN/(TN+FP)"""
+    """ f1_score = harmonic mean of the precision and recall"""
+
+    prec = precision(confusion)
+    recall = sensitivity(confusion)
+    f1 = 2 * prec * recall / (prec + recall)
+    return f1
