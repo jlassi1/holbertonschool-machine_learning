f1cfaf9910367a32c591eaba13002e98b4441cb6
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-18 16:01:32 +0100

    2-precision.py

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 """2. Precision """
+import numpy as np
+
+
+def precision(confusion):
+    """function that calculates the precision
+    for each class in a confusion matrix"""
+    """
+    TP = np.diag(confusion)
+    FP = np.sum(confusion, axis=0) - TP
+    FN = np.sum(confusion, axis=1) - TP
+    TN = confusion.sum() - (TP+FP+FN)
+    """
+    """ precision = TP/(TP+FP) = accuracy """
+    return confusion.diagonal() / confusion.sum(axis=0)
