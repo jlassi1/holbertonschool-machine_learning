b6a7631a3f8946ceb52c7d234cca35da7f367b99
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-18 15:58:13 +0100

    1-sensitivity.py

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 """1. Sensitivity """
+import numpy as np
+
+
+def sensitivity(confusion):
+    """function that calculates the sensitivity for
+    each class in a confusion matrix"""
+    """
+    TP = np.diag(confusion)
+    FP = np.sum(confusion, axis=0) - TP
+    FN = np.sum(confusion, axis=1) - TP
+    TN = confusion.sum() - (TP+FP+FN)
+    """
+    """ recall = TP/(TP+FN) = sensitivity"""
+    return confusion.diagonal() / confusion.sum(axis=1)
