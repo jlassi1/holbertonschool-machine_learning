4bca8ec1abcf61e56f458e3968ecedec22841b21
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-20 21:14:07 +0100

    4-dropout_forward_prop.py

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -7,10 +7,23 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     """
     function that conducts forward propagation using Dropout
     """
+    rnd = np.random.rand
     cache = {}
     cache["A0"] = X
     for j in range(L):
         z = np.dot(weights["W" + str(j+1)], cache[
             "A" + str(j)]) + (weights["b" + str(j+1)])
-        cache["A" + str(j+1)] = np.tanh(z)/keep_prob
+        if j == L-1:
+            """ The last layer use the softmax activation function"""
+            ACT = np.exp(z)/sum(np.exp(z))
+        else:
+            """All layers except the last use the tanh activation function"""
+            ACT = np.tanh(z)
+            d3 = (rnd(ACT.shape[0], ACT.shape[1]) < keep_prob).astype(int)
+            """ACT = np.multiply(ACT, d3)"""
+            ACT *= d3
+            ACT /= keep_prob
+            cache["D"+str(j+1)] = d3
+
+        cache["A"+str(j+1)] = ACT
     return cache
