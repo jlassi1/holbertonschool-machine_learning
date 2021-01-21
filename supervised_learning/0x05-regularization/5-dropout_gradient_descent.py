b0536f45ca6a3d6aa77482cee446d04da3d230ff
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-21 18:31:13 +0100

    ffffffffff.

---
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""5. Gradient Descent with Dropout"""
+import numpy as np
+
+
+def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
+    """
+    function  that updates the weights of a neural network with
+    Dropout regularization using gradient descent
+    """
+    m = len(Y[0])
+    dZ = cache["A" + str(L)] - Y
+    for j in range(L, 0, -1):
+        Act = cache["A" + str(j-1)]
+        dW = np.matmul(dZ, Act.T) / m
+        db = np.sum(dZ, axis=1, keepdims=True) / m
+
+        dZ = np.matmul(weights[
+            "W" + str(j)].T, dZ) * np.tanh(Act)
+
+        if j > 1:
+            dZ *= cache["D"+str(j - 1)]
+            dZ /= keep_prob
+
+        weights["W" + str(j)] -= alpha * dW
+        weights["b" + str(j)] -= alpha * db
