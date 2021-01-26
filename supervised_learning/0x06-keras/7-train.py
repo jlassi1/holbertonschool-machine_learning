5ac0517ecb620f458473e6330e0508a5889292ce
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-26 14:32:29 +0100

    7-train.py

--- supervised_learning/0x06-keras/7-train.py
+++ supervised_learning/0x06-keras/7-train.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
-""" 6. Train """
+""" 7. Train """
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels,
-                batch_size, epochs, validation_data=None,
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
                 early_stopping=False, patience=0,
-                verbose=True, shuffle=False):
-    """ early stooping """
-    if early_stopping and validation_data:
-        callback = K.callbacks.EarlyStopping(monitor='loss', patience=patience,
-                                             verbose=verbose)
-    history = network.fit(x=data, y=labels, callbacks=[callback],
+                learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, verbose=True, shuffle=False):
+    """ learning rate decay"""
+    def scheduler(epoch):
+        """ scheduler function """
+        return alpha / (1 + decay_rate * epoch)
+    callback = []
+    if validation_data:
+        if early_stopping:
+            callback.append(K.callbacks.EarlyStopping(patience=patience))
+        if learning_rate_decay:
+            callback.append(K.callbacks.LearningRateScheduler(
+                scheduler, verbose=verbose))
+    history = network.fit(x=data, y=labels, callbacks=callback,
                           epochs=epochs, batch_size=batch_size,
                           validation_data=validation_data,
                           verbose=verbose, shuffle=shuffle)
