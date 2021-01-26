4235e6bc3615d28b04829ea489ed56a6ab0e9fd1
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-26 12:07:55 +0100

    7-train.py

---
+++ supervised_learning/0x06-keras/7-train.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" 6. Train """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels,
+                batch_size, epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """ early stooping """
+    if early_stopping and validation_data:
+        callback = K.callbacks.EarlyStopping(monitor='loss', patience=patience,
+                                             verbose=verbose)
+    history = network.fit(x=data, y=labels, callbacks=[callback],
+                          epochs=epochs, batch_size=batch_size,
+                          validation_data=validation_data,
+                          verbose=verbose, shuffle=shuffle)
+    return history
