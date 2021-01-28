61821463485a93c7494468d1255b92ae3adb7fb7
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-28 14:26:40 +0100

    0-convolve_grayscale_valid.py

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""  0. Valid Convolution """
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """function  that performs a valid convolution on grayscale images"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    final_h = h - kh + 1
+    final_w = w - kw + 1
+    output = np.zeros((m, final_h, final_w))
+    for x in range(final_h):
+        for y in range(final_w):
+            output[:, x, y] = (images[:, x:kh+x, y:kw+y] * kernel
+                               ).sum(axis=(1, 2))
+    return output
