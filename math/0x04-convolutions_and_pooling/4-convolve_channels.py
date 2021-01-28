2b06ee9d2ac800f22ad8b93bbd74d9647a326993
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-28 21:28:50 +0100

    4-convolve_channels.py

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -12,8 +12,8 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     if padding == 'valid':
         final_h = int(np.floor(((h - kh)) / s_h + 1))
         final_w = int(np.floor(((w - kw)) / s_w + 1))
-        output = np.zeros((m, final_h, final_w, c))
-        image_pad = images
+        output = np.zeros((m, final_h, final_w))
+        image_pad = images.copy()
 
     if padding == "same":
         p_h = int(np.ceil(((h - 1) * s_h + kh - h) / 2))
@@ -21,10 +21,10 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
         final_h = int(np.floor((h - kh + 2 * p_h) / s_h) + 1)
         final_w = int(np.floor((w - kw + 2 * p_w) / s_w) + 1)
 
-        output = np.zeros((m, final_h, final_w, c))
+        output = np.zeros((m, final_h, final_w))
         image_pad = np.pad(
             array=images,
-            pad_width=((0,), (p_h,), (p_w,)),
+            pad_width=((0,), (p_h,), (p_w,), (0,)),
             mode="constant",
             constant_values=0)
 
@@ -33,16 +33,16 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
         final_h = int(np.floor((h - kh + 2 * p_h) / s_h) + 1)
         final_w = int(np.floor((w - kw + 2 * p_w) / s_w) + 1)
 
-        output = np.zeros((m, final_h, final_w, c))
+        output = np.zeros((m, final_h, final_w))
         image_pad = np.pad(
             array=images,
-            pad_width=((0,), (p_h,), (p_w,)),
+            pad_width=((0,), (p_h,), (p_w,), (0,)),
             mode="constant",
             constant_values=0)
 
     for x in range(final_h):
         for y in range(final_w):
-            output[:, x, y, :] = (
+            output[:, x, y] = (
                 image_pad[:, x*s_h:kh+x*s_h, y*s_w:kw+y*s_w, :] * kernel
-                               ).sum(axis=(1, 2))
-    return output.astype(np.uint8)
+                               ).sum(axis=(1, 2, 3))
+    return output
