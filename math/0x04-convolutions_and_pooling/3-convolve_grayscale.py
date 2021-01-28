fff678d4f94b4d6c8b1c35b6159311bb2bdf682e
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-28 18:00:56 +0100

    3-convolve_grayscale.py

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,35 +1,51 @@
 #!/usr/bin/env python3
-""" convolve function """
+""" 3. Strided Convolution"""
 import numpy as np
 
 
-def convolve_grayscale(images, kernel, padding='same',
-                       stride=(1, 1)):
-    """ Function that that performs a convolution on
-        grayscale images """
-    m, h, w = np.shape(images)
-    kh, kw = np.shape(kernel)
-    sh, sw = stride
-    if padding == "valid":
-        ph = 0
-        pw = 0
-    elif padding == "same":
-        """ph = (kh-kh % 2)//2
-        pw = (kw-kw % 2)//2"""
-        ph = int(((h - 1) * sh + kh - h) / 2) + 1
-        pw = int(((w - 1) * sw + kw - w) / 2) + 1
-    else:
-        ph, pw = padding
-    image = np.zeros((m, (h + 2 * ph), (w + 2 * pw)))
-    image[:, ph:h+ph, pw:w+pw] = images.copy()
-    nh = np.floor(((h + 2 * ph - kh) / stride[0]) + 1).astype(int)
-    nw = np.floor(((w + 2 * pw - kw) / stride[1]) + 1).astype(int)
-    S = np.zeros((m, nh, nw))
-    im = np.arange(0, m)
-    for i in range(nh):
-        for j in range(nw):
-            S[im, i, j] = np.sum(image[im,
-                                       sh * i:sh * i + kh,
-                                       sw * j:sw * j + kw] * kernel,
-                                 axis=(1, 2))
-    return S
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """function that performs a convolution on grayscale images """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    s_h = stride[0]
+    s_w = stride[1]
+
+    if padding == 'valid':
+        final_h = int(np.floor(((h - kh)) / s_h) + 1)
+        final_w = int(np.floor(((w - kw)) / s_w) + 1)
+        output = np.zeros((m, final_h, final_w))
+        image_pad = images
+
+    if padding == "same":
+        p_h = int(np.floor(((h - 1) * s_h + kh - h) / 2))
+        p_w = int(np.floor(((w - 1) * s_w + kw - w) / 2))
+        final_h = int(np.floor((h - kh + 2 * p_h) / s_h) + 1)
+        final_w = int(np.floor((w - kw + 2 * p_w) / s_w) + 1)
+
+        output = np.zeros((m, h, w))
+        image_pad = np.pad(
+            array=images,
+            pad_width=((0,), (p_h,), (p_w,)),
+            mode="constant",
+            constant_values=0)
+
+    if isinstance(padding, tuple):
+        p_h = padding[0]
+        p_w = padding[1]
+
+        final_h = int(np.floor((h - kh + 2 * p_h) / s_h) + 1)
+        final_w = int(np.floor((w - kw + 2 * p_w) / s_w) + 1)
+
+        output = np.zeros((m, final_h, final_w))
+        image_pad = np.pad(
+            array=images,
+            pad_width=((0,), (p_h,), (p_w,)),
+            mode="constant",
+            constant_values=0)
+
+    for x in range(final_h):
+        for y in range(final_w):
+            output[:, x, y] = (
+                image_pad[:, x*s_h:kh+x*s_h, y*s_w:kw+y*s_w] * kernel
+                               ).sum(axis=(1, 2))
+    return output
