2922220dd3d9a3b22d65bb09b56e93b6299dc905
jlassi1 <khawlajlassi1990@gmail.com>
2021-02-03 10:18:53 +0100

    0-conv_forward.py

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -12,7 +12,8 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     sh, sw = stride
     """ padding condition"""
     if padding == 'valid':
-        ph, pw = 0, 0
+        ph = 0
+        pw = 0
     if padding == 'same':
         ph = int(np.ceil(((h - 1) * sh + kh - h) / 2))
         pw = int(np.ceil(((w - 1) * sw + kw - w) / 2))
@@ -22,15 +23,15 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     nw = int((w-kh+2*pw)/sw + 1)
     output = np.empty((m, nh, nw, cn))
     """ create a pad layer"""
-    pad_lay = np.pad(A_prev, pad_width=((0,), (ph,), (pw,), (0,)),
+    pad_lay = np.pad(A_prev, pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
                      mode="constant",
-                     constant_values=0)
+                     constant_values=(0, 0))
 
     for x in range(nh):
         for y in range(nw):
             for c in range(cn):
                 pad = pad_lay[:, x*sh:kh+x*sh, y*sw:kw+y*sw, :]
                 output[:, x, y, c] = np.sum(
-                    pad*W[:, :, :, c],  axis=(1, 2, 3)) + b[:, :, :, c]
+                    pad*W[:, :, :, c], axis=(1, 2, 3)) + b[:, :, :, c]
 
     return activation(output)
