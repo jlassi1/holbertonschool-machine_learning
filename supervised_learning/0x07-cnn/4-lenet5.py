fbce7f4bbb55121d00263a51fee4a0d3526dc728
jlassi1 <khawlajlassi1990@gmail.com>
2021-02-03 18:48:07 +0100

    4-lenet5.py

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -9,9 +9,10 @@ def lenet5(x, y):
     init = tf.contrib.layers.variance_scaling_initializer(
         mode="FAN_IN")
     # Convolutional layer with 6 kernels of shape 5x5 with same padding
-    conv1 = tf.layers.Conv2D(6, (5, 5),
-                             padding='same', kernel_initializer=init,
-                             activation='relu')(x)
+    conv1 = tf.nn.relu(tf.layers.Conv2D(6, (5, 5),
+                                        padding='same',
+                                        kernel_initializer=init,
+                                        )(x))
     # Max pooling layer with kernels of shape 2x2 with 2x2 strides
     Mpool1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
     # Convolutional layer with 16 kernels of shape 5x5 with valid padding
@@ -28,18 +29,19 @@ def lenet5(x, y):
     FC2 = tf.layers.Dense(84, kernel_initializer=init,
                           activation='relu')(FC1)
     # Fully connected softmax output layer with 10 nodes
+    y_ = tf.layers.Dense(10, kernel_initializer=init)(FC2)
     y_pred = tf.layers.Dense(10, activation='softmax',
                              kernel_initializer=init)(FC2)
 
     # Define a loss function
-    loss = tf.losses.softmax_cross_entropy(y, y_pred)
+    loss = tf.losses.softmax_cross_entropy(y, y_)
     # training operation that utilizes Adam optimization
     train_op = tf.train.AdamOptimizer().minimize(loss)
 
     # predicted accuracy
     correct_prediction = tf.equal(
         tf.math.argmax(y, axis=1),
-        tf.math.argmax(y_pred, axis=1))
+        tf.math.argmax(y_, axis=1))
     acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     return y_pred, train_op, loss, acc
