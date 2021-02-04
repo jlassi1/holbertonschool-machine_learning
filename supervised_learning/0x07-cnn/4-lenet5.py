4a1c56bdcd1bd265c524c7bf480e95a1a4199fbd
jlassi1 <khawlajlassi1990@gmail.com>
2021-02-04 11:53:31 +0100

    4-lenet5.py

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -16,22 +16,21 @@ def lenet5(x, y):
     # Max pooling layer with kernels of shape 2x2 with 2x2 strides
     Mpool1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
     # Convolutional layer with 16 kernels of shape 5x5 with valid padding
-    conv2 = tf.layers.Conv2D(16, (5, 5),
-                             padding='valid', kernel_initializer=init,
-                             activation='relu')(Mpool1)
+    conv2 = tf.nn.relu(tf.layers.Conv2D(16, (5, 5),
+                                        padding='valid',
+                                        kernel_initializer=init,
+                                        )(Mpool1))
     # Max pooling layer with kernels of shape 2x2 with 2x2 strides
     Mpool2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
     # Fully connected layer with 120 nodes
     FC = tf.layers.Flatten()(Mpool2)
-    FC1 = tf.layers.Dense(120, kernel_initializer=init,
-                          activation='relu')(FC)
+    FC1 = tf.nn.relu(tf.layers.Dense(120, kernel_initializer=init)(FC))
     # Fully connected layer with 84 nodes
     FC2 = tf.layers.Dense(84, kernel_initializer=init,
                           activation='relu')(FC1)
     # Fully connected softmax output layer with 10 nodes
     y_ = tf.layers.Dense(10, kernel_initializer=init)(FC2)
-    y_pred = tf.layers.Dense(10, activation='softmax',
-                             kernel_initializer=init)(FC2)
+    y_pred = tf.nn.softmax(y_)
 
     # Define a loss function
     loss = tf.losses.softmax_cross_entropy(y, y_)
@@ -39,6 +38,7 @@ def lenet5(x, y):
     train_op = tf.train.AdamOptimizer().minimize(loss)
 
     # predicted accuracy
+    # accuracy = tf.metrics.accuracy(y, y_pred)
     correct_prediction = tf.equal(
         tf.math.argmax(y, axis=1),
         tf.math.argmax(y_, axis=1))
