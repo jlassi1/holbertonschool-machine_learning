88df1c3ae2d237d81ccd1caa933a644c43c21189
jlassi1 <khawlajlassi1990@gmail.com>
2021-02-10 14:27:31 +0100

    1-inception_network.py

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -29,25 +29,27 @@ def inception_network():
                                 strides=(2, 2), padding='same')(b3)
     a4 = inception_block(M_pool, [192, 96, 208, 16, 48, 64])
 
-    x0 = K.layers.AveragePooling2D((5, 5), strides=(3, 3), padding='valid')(a4)
-    x0 = K.layers.Conv2D(128, (1, 1), strides=(1, 1),
-                         padding="same", activation='relu')(x0)
-    x0 = K.layers.Flatten()(x0)
-    x0 = K.layers.Dense(1024, activation='relu')(x0)
-    x0 = K.layers.Dropout(0.7)(x0)
-    X0 = K.layers.Dense(1000, activation='softmax')(x0)
+    # x0 = K.layers.AveragePooling2D((5, 5), strides=(3, 3),
+    # padding='valid')(a4)
+    # x0 = K.layers.Conv2D(128, (1, 1), strides=(1, 1),
+    #                      padding="same", activation='relu')(x0)
+    # x0 = K.layers.Flatten()(x0)
+    # x0 = K.layers.Dense(1024, activation='relu')(x0)
+    # x0 = K.layers.Dropout(0.7)(x0)
+    # X0 = K.layers.Dense(1000, activation='softmax')(x0)
 
     b4 = inception_block(a4, [160, 112, 224, 24, 64, 64])
     c4 = inception_block(b4, [128, 128, 256, 24, 64, 64])
     d4 = inception_block(c4, [112, 144, 288, 32, 64, 64])
 
-    x1 = K.layers.AveragePooling2D((5, 5), strides=(3, 3), padding='valid')(d4)
-    x1 = K.layers.Conv2D(128, (1, 1), strides=(1, 1),
-                         padding="same", activation='relu')(x1)
-    x1 = K.layers.Flatten()(x1)
-    x1 = K.layers.Dense(1024, activation='relu')(x1)
-    x1 = K.layers.Dropout(0.7)(x1)
-    x1 = K.layers.Dense(1000, activation='softmax')(x1)
+    # x1 = K.layers.AveragePooling2D((5, 5), strides=(3, 3),
+    # padding='valid')(d4)
+    # x1 = K.layers.Conv2D(128, (1, 1), strides=(1, 1),
+    #                      padding="same", activation='relu')(x1)
+    # x1 = K.layers.Flatten()(x1)
+    # x1 = K.layers.Dense(1024, activation='relu')(x1)
+    # x1 = K.layers.Dropout(0.7)(x1)
+    # x1 = K.layers.Dense(1000, activation='softmax')(x1)
 
     e4 = inception_block(d4, [256, 160, 320, 23, 128, 128])
     M_pool = K.layers.MaxPool2D((3, 3),
@@ -56,8 +58,8 @@ def inception_network():
     b5 = inception_block(a5, [384, 192, 384, 48, 128, 128])
     AVG_pool = K.layers.AveragePooling2D((7, 7), strides=(1, 1),
                                          padding='valid')(b5)
+    # linear = K.layers.Flatten()(AVG_pool)
     dropout = K.layers.Dropout(0.4)(AVG_pool)
-    linear = K.layers.Flatten()(dropout)
     Y = K.layers.Dense(1000, activation='softmax')(dropout)
     # a keras model frome the input and the outputs after the inception block
     model = K.models.Model(inputs=X, outputs=Y)
