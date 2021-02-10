c45a9ec085efca614e1df90cc2af4f33bbdd6b1b
jlassi1 <khawlajlassi1990@gmail.com>
2021-02-10 20:29:09 +0100

    32

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -10,35 +10,40 @@ def inception_network():
     # create an input model with shape=(224, 224, 3)
     X = K.Input(shape=(224, 224, 3))
     # create an output of the inception network
-    Z = K.layers.Conv2D(64, (7, 7), strides=(2, 2),
+    Z = K.layers.Conv2D(filters=64, kernel_size=(7, 7), strides=2,
                         padding='same', activation='relu')(X)
 
-    Z = K.layers.MaxPool2D((3, 3),
-                           strides=(2, 2), padding='same')(Z)
-    Z = K.layers.Conv2D(64, (1, 1),
-                        strides=(1, 1), padding='same',
+    Z = K.layers.MaxPool2D(pool_size=(3, 3),
+                           strides=2, padding='same')(Z)
+    Z = K.layers.Conv2D(filters=64, kernel_size=(1, 1),
+                        strides=1, padding='same',
                         activation='relu')(Z)
-    Z = K.layers.Conv2D(192, (3, 3),
-                        strides=(1, 1), padding='same',
-                        activation='relu')(Z)
-    Z = K.layers.MaxPool2D((3, 3),
-                           strides=(2, 2), padding='same')(Z)
+    Z = K.layers.Conv2D(
+        filters=192,
+        kernel_size=(
+            3,
+            3),
+        strides=1,
+        padding='same',
+        activation='relu')(Z)
+    Z = K.layers.MaxPool2D(pool_size=(3, 3),
+                           strides=2, padding='same')(Z)
     Z = inception_block(Z, [64, 96, 128, 16, 32, 32])
     Z = inception_block(Z, [128, 128, 192, 32, 96, 64])
-    Z = K.layers.MaxPool2D((3, 3),
-                           strides=(2, 2), padding='same')(Z)
-    Z = inception_block(Z, [192, 96, 208, 16, 48, 64])
+    Z = K.layers.MaxPool2D(pool_size=(3, 3),
+                           strides=2, padding='same')(Z)
 
+    Z = inception_block(Z, [192, 96, 208, 16, 48, 64])
     Z = inception_block(Z, [160, 112, 224, 24, 64, 64])
     Z = inception_block(Z, [128, 128, 256, 24, 64, 64])
     Z = inception_block(Z, [112, 144, 288, 32, 64, 64])
+    Z = inception_block(Z, [256, 160, 320, 32, 128, 128])
 
-    Z = inception_block(Z, [256, 160, 320, 23, 128, 128])
-    Z = K.layers.MaxPool2D((3, 3),
+    Z = K.layers.MaxPool2D(pool_size=(3, 3),
                            strides=(2, 2), padding='same')(Z)
     Z = inception_block(Z, [256, 160, 320, 32, 128, 128])
     Z = inception_block(Z, [384, 192, 384, 48, 128, 128])
-    Z = K.layers.AveragePooling2D((7, 7), strides=1,
+    Z = K.layers.AveragePooling2D(pool_size=(7, 7), strides=1,
                                   padding='valid')(Z)
 
     Z = K.layers.Dropout(0.4)(Z)
