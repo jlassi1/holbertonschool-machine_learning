c5e7451f5d460ea9f27928f23585adf61c4532e3
jlassi1 <khawlajlassi1990@gmail.com>
2021-02-10 20:31:13 +0100

    2-identity_block.py

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -11,27 +11,32 @@ def identity_block(A_prev, filters):
     # initialize weights with he normal
     init = K.initializers.he_normal()
     # Save the input value.
-    A_shortcut = A_prev
+    # A_shortcut = A_prev
     # First component of main path
     Z = K.layers.Conv2D(
-        F11, (1, 1), padding='same', kernel_initializer=init)(A_prev)
+        filters=F11,
+        kernel_size=(
+            1,
+            1),
+        padding='same',
+        kernel_initializer=init)(A_prev)
     Z = K.layers.BatchNormalization(axis=3)(Z)
     Z = K.layers.Activation('relu')(Z)
     # Second component of main path
     Z = K.layers.Conv2D(
-        F3, kernel_size=(
+        filters=F3, kernel_size=(
             3, 3), padding='same', kernel_initializer=init)(Z)
     Z = K.layers.BatchNormalization(axis=3)(Z)
     Z = K.layers.Activation('relu')(Z)
     # Third component of main path
     Z = K.layers.Conv2D(
-        F12, kernel_size=(
+        filters=F12, kernel_size=(
             1, 1), padding='same', kernel_initializer=init)(Z)
     X = K.layers.BatchNormalization(axis=3)(Z)
 
     # Final step: Add shortcut value to main path, and pass it through a RELU
     # activation
-    X = K.layers.Add()([A_shortcut, X])
+    X = K.layers.Add()([A_prev, X])
     X = K.layers.Activation('relu')(X)
 
     return X
