5dcc24269277a6ce0b623ba1f70184c464f0d65e
jlassi1 <khawlajlassi1990@gmail.com>
2021-02-11 14:43:09 +0100

    check 6

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -1,42 +1,28 @@
 #!/usr/bin/env python3
-""" 2. Identity Block"""
+""" identity_block """
 import tensorflow.keras as K
 
 
 def identity_block(A_prev, filters):
-    """function that builds an identity block as described
-    in Deep Residual Learning for Image Recognition (2015)"""
-    # Retrieve Filters
+    """ Function that builds an identity block as
+        described in Deep Residual Learning for Image
+        Recognition (2015) """
     F11, F3, F12 = filters
-    # initialize weights with he normal
-    # init = K.initializers.he_normal()
-    # Save the input value.
-    A_shortcut = A_prev
-    # First component of main path
-    Z = K.layers.Conv2D(
-        filters=F11,
-        kernel_size=(
-            1,
-            1),
-        padding='same',
-        kernel_initializer='he_normal')(A_prev)
-    Z = K.layers.BatchNormalization()(Z)
-    Z = K.layers.Activation('relu')(Z)
-    # Second component of main path
-    Z = K.layers.Conv2D(
-        filters=F3, kernel_size=(
-            3, 3), padding='same', kernel_initializer='he_normal')(Z)
-    Z = K.layers.BatchNormalization()(Z)
-    Z = K.layers.Activation('relu')(Z)
-    # Third component of main path
-    Z = K.layers.Conv2D(
-        filters=F12, kernel_size=(
-            1, 1), padding='same', kernel_initializer='he_normal')(Z)
-    X = K.layers.BatchNormalization()(Z)
-
-    # Final step: Add shortcut value to main path, and pass it through a RELU
-    # activation
-    X = K.layers.Add()([A_shortcut, X])
-    X = K.layers.Activation('relu')(X)
-
-    return X
+    he_init = K.initializers.he_normal()
+    conv_1 = K.layers.Conv2D(filters=F11, kernel_size=(1, 1),
+                             padding='same',
+                             kernel_initializer=he_init)(A_prev)
+    norm_1 = K.layers.BatchNormalization(axis=3)(conv_1)
+    Act1 = K.layers.Activation('relu')(norm_1)
+    conv_2 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3),
+                             padding='same',
+                             kernel_initializer=he_init)(Act1)
+    norm_2 = K.layers.BatchNormalization(axis=3)(conv_2)
+    Act2 = K.layers.Activation('relu')(norm_2)
+    conv_3 = K.layers.Conv2D(filters=F12, kernel_size=(1, 1),
+                             padding='same',
+                             kernel_initializer=he_init)(Act2)
+    norm_3 = K.layers.BatchNormalization(axis=3)(conv_3)
+    add1 = K.layers.Add()([norm_3, A_prev])
+    res = K.layers.Activation('relu')(add1)
+    return res
