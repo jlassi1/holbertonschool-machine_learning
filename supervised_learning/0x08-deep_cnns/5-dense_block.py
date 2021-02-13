01b76fc9d5f914d27ab940209ac85c2d769c4724
jlassi1 <khawlajlassi1990@gmail.com>
2021-02-13 20:56:25 +0100

    5-dense_block.py

--- supervised_learning/0x08-deep_cnns/5-dense_block.py
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -7,29 +7,43 @@ def dense_block(X, nb_filters, growth_rate, layers):
     """ function  that builds a dense block as described
     in Densely Connected Convolutional Networks"""
     for i in range(layers):
-        each_layer = conv_layer(X, growth_rate)
-        X = K.layers.concatenate([X, each_layer])
+        conv_x = K.layers.BatchNormalization()(X)
+        conv_x = K.layers.Activation('relu')(conv_x)
+        conv_x = K.layers.Conv2D(
+            4 * growth_rate,
+            (1,
+             1),
+            kernel_initializer='he_normal',
+            padding='same')(conv_x)
+        conv_x = K.layers.BatchNormalization()(conv_x)
+        conv_x = K.layers.Activation('relu')(conv_x)
+        conv_x = K.layers.Conv2D(
+            growth_rate,
+            (3,
+             3),
+            kernel_initializer='he_normal',
+            padding='same')(conv_x)
+        X = K.layers.concatenate([X, conv_x])
         nb_filters += growth_rate
 
     return X, nb_filters
 
-
-def conv_layer(conv_x, filters):
-    """the bottleneck layers used for DenseNet-B"""
-    conv_x = K.layers.BatchNormalization()(conv_x)
-    conv_x = K.layers.Activation('relu')(conv_x)
-    conv_x = K.layers.Conv2D(
-        4 * filters,
-        (1,
-         1),
-        kernel_initializer='he_normal',
-        padding='same')(conv_x)
-    conv_x = K.layers.BatchNormalization()(conv_x)
-    conv_x = K.layers.Activation('relu')(conv_x)
-    conv_x = K.layers.Conv2D(
-        filters,
-        (3,
-         3),
-        kernel_initializer='he_normal',
-        padding='same')(conv_x)
-    return conv_x
+# def conv_layer(conv_x, filters):
+#     """the bottleneck layers used for DenseNet-B"""
+#     conv_x = K.layers.BatchNormalization()(conv_x)
+#     conv_x = K.layers.Activation('relu')(conv_x)
+#     conv_x = K.layers.Conv2D(
+#         4 * filters,
+#         (1,
+#          1),
+#         kernel_initializer='he_normal',
+#         padding='same')(conv_x)
+#     conv_x = K.layers.BatchNormalization()(conv_x)
+#     conv_x = K.layers.Activation('relu')(conv_x)
+#     conv_x = K.layers.Conv2D(
+#         filters,
+#         (3,
+#          3),
+#         kernel_initializer='he_normal',
+#         padding='same')(conv_x)
+#     return conv_x
