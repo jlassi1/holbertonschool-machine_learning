5e576ed44ae89ccd2ea92150f2cb86172824876e
jlassi1 <khawlajlassi1990@gmail.com>
2021-02-20 21:23:10 +0100

    0-transfer.py

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -16,22 +16,23 @@ def preprocess_data(X, Y):
 
 
 def base_model():
-    # create an input model with shape=(224, 224, 3)
+    """ create a base model for CNNS to cifar10 dataset"""
+    # load the cifar10 data the training and testing data
     (X, Y), (x_test, y_test) = K.datasets.cifar10.load_data()
+    # preprocessing the data
     X_p, Y_p = preprocess_data(X, Y)
     x_t, y_t = preprocess_data(x_test, y_test)
-
+    # create a model
     model = K.Sequential()
-    init = 'he_uniform'
+    # initialize the kernal
+    init = 'glorot_normal'
+    # choose the densenet201 models 
     base_model = K.applications.DenseNet201(
         include_top=False, weights='imagenet')
-    # base_model.trainable = False
-    for layer in base_model.layers:
-        if 'conv5' in layer.name:
-            layer.trainable = True
-        else:
-            layer.trainable = False
-
+    # freeze layers
+    base_model.trainable = False
+    
+    # resize the input size to (224,224) model predefine size
     inputLayer = K.layers.Lambda(lambda input_img: K.backend.resize_images
                                  (input_img, height_factor=7,
                                   width_factor=7,
@@ -39,38 +40,37 @@ def base_model():
 
     model.add(inputLayer)
     model.add(base_model)
+
     model.add(K.layers.Flatten())
     model.add(K.layers.BatchNormalization())
     model.add(K.layers.Dense(1000, activation='relu', kernel_initializer=init))
-    model.add(K.layers.Dropout(0.7))
-    model.add(K.layers.BatchNormalization())
-    model.add(K.layers.Dense(256, activation='relu', kernel_initializer=init))
     model.add(K.layers.Dropout(0.6))
     model.add(K.layers.BatchNormalization())
-    model.add(K.layers.Dense(128, activation='relu', kernel_initializer=init))
+    model.add(K.layers.Dense(256, activation='relu', kernel_initializer=init))
     model.add(K.layers.Dropout(0.4))
     model.add(K.layers.BatchNormalization())
-
+    model.add(K.layers.Dense(128, activation='relu', kernel_initializer=init))
+    model.add(K.layers.Dropout(0.2))
+    model.add(K.layers.BatchNormalization())
     model.add(K.layers.Dense(64, activation='relu', kernel_initializer=init))
-
     model.add(
         K.layers.Dense(
             num_classes,
             activation='softmax',
             kernel_initializer=init))
-
-    model.compile(loss="binary_crossentropy",
-                  optimizer=K.optimizers.Adam(1e-06),
+    # compile the model 
+    model.compile(loss="categorical_crossentropy",
+                  optimizer=K.optimizers.Adam(),
                   metrics=['accuracy'])
     hist = model.fit(
         X_p,
         Y_p,
-        batch_size=128,
-        epochs=30,
+        batch_size=300,
+        epochs=10,
         validation_data=(
             x_t,
             y_t))
-    # STEP 5 : Evaluating
+    #Evaluating
     modelLoss, modelAccuracy = model.evaluate(x_t, y_t)
     print('model Loss is {}'.format(modelLoss))
     print('model Accuracy is {}'.format(modelAccuracy))
@@ -79,5 +79,4 @@ def base_model():
 
 
 if __name__ == "__main__":
-    """it still has work last Accu 0.73 """
     base_model()
