886ce2c0f192712ee9443e8204884cdaab147119
jlassi1 <khawlajlassi1990@gmail.com>
2021-02-21 20:27:35 +0100

    0-transfer.py

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -17,48 +17,61 @@ def preprocess_data(X, Y):
 
 def base_model():
     """ create a base model for CNNS to cifar10 dataset"""
-    # load the cifar10 data the training and testing data
+    # load the Cifar10 dataset, 50,000 training images
+    # and 10,000 test images (here used as validation data)
     (X, Y), (x_test, y_test) = K.datasets.cifar10.load_data()
-    # preprocessing the data
+    # preprocess the data using the application's preprocess_input method and
+    # convert the labels to one-hot encodings
     X_p, Y_p = preprocess_data(X, Y)
     x_t, y_t = preprocess_data(x_test, y_test)
+
     # create a model
     model = K.Sequential()
     # initialize the kernal
-    init = 'glorot_normal'
-    # choose the densenet201 models 
-    base_model = K.applications.DenseNet201(
-        include_top=False, weights='imagenet')
-    # freeze layers
-    base_model.trainable = False
-    
+    init = 'he_normal'
     # resize the input size to (224,224) model predefine size
     inputLayer = K.layers.Lambda(lambda input_img: K.backend.resize_images
                                  (input_img, height_factor=7,
                                   width_factor=7,
                                   data_format='channels_last'))
+    # choose the densenet201 models
+    base_model = K.applications.DenseNet201(include_top=False,
+                                            weights='imagenet',
+                                            input_tensor=resized_images,
+                                            input_shape=(224, 224, 3),
+                                            pooling='max')
+    # freeze layers
+    base_model.trainable = False
 
-    model.add(inputLayer)
+    # create our model
+    model = K.Sequential()
     model.add(base_model)
 
     model.add(K.layers.Flatten())
     model.add(K.layers.BatchNormalization())
-    model.add(K.layers.Dense(1000, activation='relu', kernel_initializer=init))
+    model.add(K.layers.Dense(units=256,
+                             activation='relu',
+                             kernel_initializer=initializer,
+                             kernel_regularizer=K.regularizers.l2()))
     model.add(K.layers.Dropout(0.6))
     model.add(K.layers.BatchNormalization())
-    model.add(K.layers.Dense(256, activation='relu', kernel_initializer=init))
+    model.add(K.layers.Dense(units=128,
+                             activation='relu',
+                             kernel_initializer=initializer,
+                             kernel_regularizer=K.regularizers.l2()))
     model.add(K.layers.Dropout(0.4))
     model.add(K.layers.BatchNormalization())
-    model.add(K.layers.Dense(128, activation='relu', kernel_initializer=init))
-    model.add(K.layers.Dropout(0.2))
-    model.add(K.layers.BatchNormalization())
-    model.add(K.layers.Dense(64, activation='relu', kernel_initializer=init))
+    model.add(K.layers.Dense(64, activation='relu',
+                             kernel_initializer=initializer,
+                             kernel_regularizer=K.regularizers.l2()))
     model.add(
         K.layers.Dense(
-            num_classes,
+            units=num_classes,
             activation='softmax',
-            kernel_initializer=init))
-    # compile the model 
+            kernel_initializer=initializer,
+            kernel_regularizer=K.regularizers.l2()))
+
+    # compile the model
     model.compile(loss="categorical_crossentropy",
                   optimizer=K.optimizers.Adam(),
                   metrics=['accuracy'])
@@ -66,15 +79,15 @@ def base_model():
         X_p,
         Y_p,
         batch_size=300,
-        epochs=10,
+        epochs=5,
         validation_data=(
             x_t,
             y_t))
-    #Evaluating
+    # Evaluating
     modelLoss, modelAccuracy = model.evaluate(x_t, y_t)
     print('model Loss is {}'.format(modelLoss))
     print('model Accuracy is {}'.format(modelAccuracy))
-
+    # save the model with name cifar10.h5
     model.save("cifar10.h5")
 
 
