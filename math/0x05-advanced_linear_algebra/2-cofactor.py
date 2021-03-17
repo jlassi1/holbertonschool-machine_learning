3b31c37b17b12c0aea4400278680383a314eb112
jlassi1 <khawlajlassi1990@gmail.com>
2021-03-17 14:50:06 +0100

    2-cofactor.py

---
+++ math/0x05-advanced_linear_algebra/2-cofactor.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+""" 2. Cofactor  """
+
+
+def determinant(matrix):
+    """function that calculates the determinant of a matrix"""
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError('matrix must be a list of lists')
+    if not all(isinstance(i, list) for i in matrix):
+        raise TypeError('matrix must be a list of lists')
+    if matrix == [[]] or len(matrix[0]) == 0:
+        return 1
+    if len(matrix[0]) == 1:
+        return matrix[0][0]
+    if len(matrix) != len(matrix[0]):
+        raise ValueError('matrix must be a square matrix')
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    det = 0
+    n = len(matrix)
+    for i in range(n):
+        x = [[matrix[i][j] for j in range(n)] for i in range(n)]
+        x.pop(0)
+        for m in range(n - 1):
+            x[m].pop(i)
+        det += ((-1)**i) * matrix[0][i] * determinant(x)
+    return det
+
+
+def cofactor(matrix):
+    """function that calculates the cofactor  matrix of a matrix"""
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError('matrix must be a list of lists')
+    if not all(isinstance(i, list) for i in matrix):
+        raise TypeError('matrix must be a list of lists')
+    if len(matrix) != len(matrix[0]) or matrix == []:
+        raise ValueError('matrix must be a non-empty square matrix')
+    if len(matrix[0]) == 1 or matrix == [[]] or len(matrix[0]) == 0:
+        return [[1]]
+    if len(matrix) == 2:
+        return [[matrix[1][1], matrix[1][0]], [matrix[0][1], matrix[0][0]]]
+    mi = []
+    n = len(matrix)
+    y = 0
+    for j in range(n):
+        r = []
+        for i in range(n):
+            x = [[matrix[i][j] for j in range(n)] for i in range(n)]
+            x.pop(j)
+            for m in range(n - 1):
+                x[m].pop(i)
+            y = (-1)**(i + j) * determinant(x)
+            r.append(y)
+        mi.append(list(r))
+    return mi
