c16b85ded4d90ca3d14cbb90e67389f1c2baa8d8
jlassi1 <khawlajlassi1990@gmail.com>
2021-03-17 11:56:35 +0100

    0-determinant.py

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -10,12 +10,12 @@ def determinant(matrix):
         raise TypeError('matrix must be a list of lists')
     if matrix == [[]]:
         return 1
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+    if len(matrix) == 1 and len(matrix[0]) == 1:
+        return matrix[0][0]
     if len(matrix) != len(matrix[0]):
         raise ValueError('matrix must be a square matrix')
-    if len(matrix[0]) == 0:
-        return 1
-    if len(matrix[0]) == 1:
-        return matrix[0][0]
     if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     det = 0
