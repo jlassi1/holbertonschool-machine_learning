61a2b3ae9a5c0fd1a03a2a617b46243719069295
jlassi1 <khawlajlassi1990@gmail.com>
2021-03-17 11:35:43 +0100

    0-determinant.py

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" 0. Determinant  """
+import copy
+
+
+def determinant(matrix):
+    """function that calculates the determinant of a matrix"""
+    if not isinstance(matrix, list) and not isinstance(matrix[0], list):
+        raise TypeError('matrix must be a list of lists')
+    if not all(isinstance(i, list) for i in matrix):
+        raise TypeError('matrix must be a list of lists')
+    if matrix == [[]]:
+        return 1
+    if len(matrix) != len(matrix[0]):
+        raise ValueError('matrix must be a square matrix')
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+    if len(matrix) == 1 and len(matrix[0]) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    det = 0
+    n = len(matrix)
+    for i in range(n):
+        x = [[matrix[i][j] for j in range(n)] for i in range(n)]
+        x.pop(0)
+        for m in range(n - 1):
+            x[m].pop(i)
+        det += (-1)**i * matrix[0][i] * determinant(x)
+    return det
