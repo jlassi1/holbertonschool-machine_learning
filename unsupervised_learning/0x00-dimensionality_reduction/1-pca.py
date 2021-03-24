d6cceb21cc8be3b7a8286ff2b9ad486f5d20d18c
jlassi1 <khawlajlassi1990@gmail.com>
2021-03-24 17:40:34 +0100

    1-pca.py

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+""" 1. PCA v2  """
+import numpy as np
+
+
+def pca(X, ndim):
+    """ Function that performs PCA on a dataset: """
+    X = X - np.mean(X, axis=0)
+    W = np.linalg.svd(X)[2][:ndim, :].T
+    return np.matmul(X, W)
