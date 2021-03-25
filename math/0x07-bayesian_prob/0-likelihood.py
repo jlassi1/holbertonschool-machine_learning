92d61c4e7970e90b16d8454878a1a3ab1004368a
jlassi1 <khawlajlassi1990@gmail.com>
2021-03-25 17:20:25 +0100

    0-likelihood.py

---
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" 0. Likelihood """
+
+
+def likelihood(x, n, P):
+    """function that calculates the likelihood of obtaining
+    this data given various hypothetical probabilities
+    of developing severe side effects"""
+    
