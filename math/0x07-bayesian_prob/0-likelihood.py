55458dcacce887c73375f3e14f0ca07d8ebf9e4b
jlassi1 <khawlajlassi1990@gmail.com>
2021-03-26 11:39:08 +0100

    0-likelihood.py

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -7,14 +7,14 @@ def likelihood(x, n, P):
     """function that calculates the likelihood of obtaining
     this data given various hypothetical probabilities
     of developing severe side effects"""
-    if not isinstance(n, int) or n < 0:
+    if not isinstance(n, int) or n <= 0:
         raise ValueError('n must be a positive integer')
     if not isinstance(x, int) or x < 0:
         raise ValueError('x must be an integer \
         that is greater than or equal to 0')
     if x > n:
         raise ValueError('x cannot be greater than n')
-    if not isinstance(P, np.ndarray) or P.size == 1:
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError('P must be a 1D numpy.ndarray')
     if (P > 1).any() or (P < 0).any():
         raise ValueError('All values in P must be in the range [0, 1]')
