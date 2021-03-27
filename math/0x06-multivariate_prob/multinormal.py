37575ca55259f77bfe16b44f69d0de829f3216d2
jlassi1 <khawlajlassi1990@gmail.com>
2021-03-27 23:28:11 +0100

    multinormal.py

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -15,3 +15,16 @@ class MultiNormal:
         self.mean = data.mean(axis=1, keepdims=True)
         Xi = data - self.mean
         self.cov = np.dot(Xi, Xi.T) / (data.shape[1] - 1)
+
+    def pdf(self, x):
+        """ Probability density Function """
+        d = self.mean.shape[0]
+        if not isinstance(x, np.ndarray):
+            raise TypeError('x must be a numpy.ndarray')
+        if x.ndim != 2 or (x.shape[0] != d)or (x.shape[1] != 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        arg_mean = x - self.mean
+        invsqrtdet = np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov))
+        invcov = np.linalg.inv(self.cov)
+        exp = np.exp(-(np.dot(np.dot(arg_mean.T, invcov), arg_mean)) / 2)
+        return (1 / invsqrtdet * exp)[0][0]
