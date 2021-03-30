ec88131cec4a4ce277c8f108c42c320f554e4219
jlassi1 <khawlajlassi1990@gmail.com>
2021-03-30 12:44:34 +0100

    kmeans

---
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""1. K-means  """
+import numpy as np
+
+
+def initialize(X, k):
+    """function that initializes cluster centroids for K-mean"""
+    if not isinstance(k, int) or k <= 0:
+        return None
+    try:
+        n, d = X.shape
+        points = np.random.uniform(
+            low=np.min(X, axis=0), high=np.max(X, axis=0), size=(k, d))
+    except Exception:
+        return None
+    return points
+
+    return Centroids, clss
+
+
+def dist(data, centers):
+    """function that calcules the distant between centre and data"""
+    distance = np.sqrt(np.sum(
+        (np.array(centers) - data[:, None, :])**2, axis=2))
+    return distance
+
+
+def kmeans(X, k, iterations=1000):
+    """function that performs K-means on a dataset"""
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
+
+    centroids = initialize(X, k)
+    if centroids is None:
+        return None, None
+    closest = np.argmin(dist(X, centroids), axis=1)
+    for i in range(iterations):
+        cp = centroids.copy()
+        for j in range(k):
+            if X[np.where(closest == j)].size == 0:
+                centroids[j] = initialize(X, 1)
+            else:
+                centroids[j] = X[np.where(closest == j)].mean(axis=0)
+        closest = np.argmin(dist(X, centroids), axis=1)
+        if np.array_equal(cp, centroids):
+            break
+
+    return centroids, closest
