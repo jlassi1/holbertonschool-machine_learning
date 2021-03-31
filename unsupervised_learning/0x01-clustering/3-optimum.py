311014d22dc03e31f3c909bd631649c7f03043a2
jlassi1 <khawlajlassi1990@gmail.com>
2021-03-31 12:32:03 +0100

    3-optimum.py

---
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""3. Optimize k """
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """function that tests for the optimum number of clusters by variance"""
+    if not isinstance(kmin, int) or kmin < 1:
+        return None, None
+    if not isinstance(kmax, int) or kmax < kmin - 1:
+        return None, None
+    try:
+        results = []
+        d_vars = []
+        c1, clss1 = kmeans(X, kmin, iterations)
+        var_min = variance(X, c1)
+        for k in range(kmin, kmax + 1):
+            C, clss = kmeans(X, k, iterations)
+            results.append((C, clss))
+            var = variance(X, C)
+            d_vars.append(var_min - var)
+    except Exception:
+        return None, None
+    return results, d_vars
