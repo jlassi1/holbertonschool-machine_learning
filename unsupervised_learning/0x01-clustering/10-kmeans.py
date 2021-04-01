47cf5827caa398ecfbc1d9a5ef0ec5d98823f88c
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-01 18:42:53 +0100

    10-kmeans.py

--- unsupervised_learning/0x01-clustering/10-kmeans.py
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -3,8 +3,14 @@
 import numpy as np
 import sklearn.cluster
 
+# whyyyyy
+# def kmeans(X, k):
+#     """function that performs K-means on a dataset"""
+#     kmeans = sklearn.cluster.KMeans(n_clusters=k).fit(X)
+#     return kmeans.cluster_centers_, kmeans.labels_
+
 
 def kmeans(X, k):
     """function that performs K-means on a dataset"""
-    kmeans = sklearn.cluster.KMeans(n_clusters=k).fit(X)
-    return kmeans.cluster_centers_, kmeans.labels_
+    centroids, clss, inertia = sklearn.cluster.k_means(X, k)
+    return centroids, clss
