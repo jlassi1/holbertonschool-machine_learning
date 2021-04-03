16cde6106e7a1a375dab138241cb311cfd7c1b22
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-03 23:16:36 +0100

    5-pdf.py

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -6,10 +6,13 @@ import numpy as np
 def pdf(X, m, S):
     """function that calculates the probability density
     function of a Gaussian distribution"""
-    d = X.shape[1]
-    y = np.sqrt((2*np.pi)**(d)*np.linalg.det(S))
-    diff = (X - m).T
-    z = np.exp(-0.5*np.dot(np.dot(diff.T, np.linalg.inv(S)), diff)).diagonal()
-    P = z/y
-    PDF = np.where(P < 1e-300, 1e-300, P)
-    return PDF
+    try:
+        d = X.shape[1]
+        y = np.sqrt((2*np.pi)**(d)*np.linalg.det(S))
+        diff = (X - m).T
+        z = np.exp(-0.5*np.dot(np.dot(diff.T, np.linalg.inv(S)), diff)).diagonal()
+        P = z/y
+        PDF = np.where(P < 1e-300, 1e-300, P)
+        return PDF
+    except Exception:
+        return None
