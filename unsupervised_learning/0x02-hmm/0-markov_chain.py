1a551a61cff02325f3d5e80f992dc34e7aa10d59
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-05 15:14:09 +0100

    0-markov_chain.py

---
+++ unsupervised_learning/0x02-hmm/0-markov_chain.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""0. Markov Chain """
+import numpy as np
+
+
+def markov_chain(P, s, t=1):
+    """function that determines the probability of a markov chain
+    being in a particular state after a specified number of iterations"""
+    state = s
+    for x in range(t):
+        state = np.dot(state, P)
+    return state
