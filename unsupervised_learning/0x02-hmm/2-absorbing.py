bfe9cb292bbc7dff8b4263a3db8d068302e478c2
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-07 15:48:01 +0100

    2-absorbing.py

---
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" 2. Absorbing Chains  """
+import numpy as np
+
+
+def absorbing(P):
+    """function that determines if a markov chain is absorbing"""
+    try:
+        n = P.shape[0]
+        if (P == np.eye(n)).all():
+            return True
+        diag = np.where(np.diag(P) == 1, 1, 0)
+        if not np.any(diag == 1):
+            return False
+
+        d, v = np.linalg.eig(P)
+        # print( np.diag(d == 1).astype(int))
+        # print(np.linalg.inv(v))
+        P_bar = v @ np.diag(d == 1).astype(int) @ np.linalg.inv(v)
+        # print(v @ np.diag(d >= 1).astype(int) @ np.linalg.inv(v) )
+        # print(int(np.ceil(np.max(P_bar.sum()))))
+        if int(np.ceil(np.max(P_bar.sum()))) != n:
+            return False
+        return True
+
+    except Exception:
+        return False
