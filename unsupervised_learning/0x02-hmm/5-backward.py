7f98f8091f3ca3f2ede00ed4f4dd83df4893c965
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-10 20:49:18 +0100

    5-backward.py

--- unsupervised_learning/0x02-hmm/5-backward.py
+++ unsupervised_learning/0x02-hmm/5-backward.py
@@ -9,7 +9,6 @@ def backward(Observation, Emission, Transition, Initial):
         T = Observation.shape[0]
         N, _ = Emission.shape
         Beta = np.ones((N, T))
-        # Beta[:, T - 1] = 1
 
         for t in range(T - 2, -1, -1):
             for s in range(N):
