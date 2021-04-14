a494f9efb4f388d4e2243df64b1c7ef81cc1f402
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-14 12:20:18 +0100

    first_commit

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" 0. Initialize Gaussian Process """
+import numpy as np
+
+
+class GaussianProcess:
+    """class that represents a noiseless 1D Gaussian process"""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """initialization"""
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        """function that calculates the covariance kernel
+        matrix between two matrices"""
+        # sqdist = np.sum(X1**2, 1).reshape(-1, 1) +
+        # np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        # return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+        K = np.zeros((X1.shape[0], X2.shape[0]))
+        for i, x1 in enumerate(X1):
+            for j, x2 in enumerate(X2):
+                K[i, j] = np.exp(-0.5 / self.l**2 * (x1 - x2)**2)
+        return self.sigma_f**2 * K
