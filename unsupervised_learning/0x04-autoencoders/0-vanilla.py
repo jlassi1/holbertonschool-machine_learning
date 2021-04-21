f702c15b621f1ae0192acdbe978fd06077b08777
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-21 23:56:13 +0100

    0-vanilla.py

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -9,7 +9,7 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     input_img = keras.Input(shape=(input_dims,))
 
     for i in hidden_layers:
-        if i == hidden_layers[0]:
+        if i is hidden_layers[0]:
             encoded = keras.layers.Dense(i, activation='relu')(input_img)
         else:
             encoded = keras.layers.Dense(i, activation='relu')(encoded)
@@ -19,9 +19,9 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     encoder = keras.Model(input_img, latent)
 
     decoder_input = keras.Input(shape=(latent_dims,))
-    # print(hidden_layers[::-1])
+
     for i in hidden_layers[::-1]:
-        if i == hidden_layers[-1]:
+        if i is hidden_layers[-1]:
             decoded = keras.layers.Dense(i, activation='relu')(decoder_input)
         else:
             decoded = keras.layers.Dense(i, activation='relu')(decoded)
