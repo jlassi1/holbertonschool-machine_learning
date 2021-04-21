da66c6255d5375080a0b37e618cb37dd5f97d0db
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-21 14:47:23 +0100

    2-convolutional.py

--- unsupervised_learning/0x04-autoencoders/2-convolutional.py
+++ unsupervised_learning/0x04-autoencoders/2-convolutional.py
@@ -6,9 +6,8 @@ import tensorflow.keras as keras
 def autoencoder(input_dims, filters, latent_dims):
     """function that creates a convolutional autoencoder"""
     input_img = keras.Input(shape=input_dims)
-
     for i in filters:
-        if i == filters[0]:
+        if i is filters[0]:
             encoded = keras.layers.Conv2D(
                 i, (3, 3), activation='relu', padding='same')(input_img)
         else:
@@ -19,22 +18,20 @@ def autoencoder(input_dims, filters, latent_dims):
 
     encoder = keras.Model(input_img, encoded)
 
-    decoder_input = keras.Input(shape=latent_dims)
-    # if i == filters[-1]:
+    decoded = keras.Input(shape=latent_dims)
     decoded = keras.layers.Conv2D(
                 filters[-1], (3, 3),
                 activation='relu', padding='same')(decoder_input)
-    # decoded = keras.layers.UpSampling2D((2, 2))(decoded)
-    for i in filters[::-1]:
-        if i == filters[0]:
-            decoded = keras.layers.Conv2D(
-                i, (3, 3), padding='valid', activation='relu')(decoded)
-            # decoded = keras.layers.UpSampling2D((2, 2))(decoded)
-        else:
-            decoded = keras.layers.Conv2D(
-                i, (3, 3), activation='relu', padding='same')(decoded)
+    decoded = keras.layers.UpSampling2D((2, 2))(decoded)
+
+    for i in range(len(filters)-2, 0, -1):
+        decoded = keras.layers.Conv2D(
+                filters[i], (3, 3), padding='same', activation='relu')(decoded)
         decoded = keras.layers.UpSampling2D((2, 2))(decoded)
 
+    decoded = keras.layers.Conv2D(filters[0], (3, 3),
+                                  padding='valid', activation='relu')(decoded)
+    decoded = keras.layers.UpSampling2D((2, 2))(decoded)
     decoded = keras.layers.Conv2D(
         input_dims[-1], (3, 3), activation='sigmoid', padding='same')(decoded)
 
