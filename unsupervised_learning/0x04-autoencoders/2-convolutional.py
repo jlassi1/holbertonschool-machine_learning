92a3a86080415d68cae123caea01e8fcf84e1dfb
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-21 14:07:14 +0100

    2-convolutional.py

--- unsupervised_learning/0x04-autoencoders/2-convolutional.py
+++ unsupervised_learning/0x04-autoencoders/2-convolutional.py
@@ -22,23 +22,26 @@ def autoencoder(input_dims, filters, latent_dims):
     encoder = keras.Model(input_img, encoded)
 
     decoder_input = keras.Input(shape=latent_dims)
+    # if i == filters[-1]:
     decoded = keras.layers.Conv2D(
-        filters[-1], (3, 3), activation='relu', padding='same')(decoder_input)
+                filters[-1], (3, 3), activation='relu', padding='same')(decoder_input)
     decoded = keras.layers.UpSampling2D((2, 2))(decoded)
-    for i in range(len(filters) - 2, 0, -1):
-        decoded = keras.layers.Conv2D(
-            filters[i], (3, 3), activation='relu', padding='same')(decoded)
-        decoded = keras.layers.UpSampling2D((2, 2))(decoded)
+    for i in filters[::-1]:
+        if i == filters[0]:
+            decoded = keras.layers.Conv2D(
+                i, (3, 3), padding='valid', activation='relu')(decoded)
+            # decoded = keras.layers.UpSampling2D((2, 2))(decoded)
+        else:
+            decoded = keras.layers.Conv2D(
+                i, (3, 3), activation='relu', padding='same')(decoded)
+            decoded = keras.layers.UpSampling2D((2, 2))(decoded)
 
     decoded = keras.layers.Conv2D(
-        filters[0], (3, 3), padding='valid', activation='relu')(decoded)
-    decoded = keras.layers.UpSampling2D((2, 2))(decoded)
-    decoded = keras.layers.Conv2D(
-        input_dims[-1], (3, 3), activation='sigmoid', padding='same')(decoded)
+        input_dims[-1], (3, 3), activation='sigmoid')(decoded)
 
     decoder = keras.Model(decoder_input, decoded)
 
-    auto = keras.Model(input_img, decoder(encoded))
-    auto.compile("Adam", 'binary_crossentropy')
+    auto = keras.Model(input_img, decoder(encoder(input_img)))
+    auto.compile(loss='binary_crossentropy', optimizer='adam')
 
     return encoder, decoder, auto
