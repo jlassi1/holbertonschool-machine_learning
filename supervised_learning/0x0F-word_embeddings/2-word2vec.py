429a81ac599e98abf5d9e473a43259380fd95368
jlassi1 <khawlajlassi1990@gmail.com>
2021-05-11 23:04:45 +0100

    2-word2vec.py

--- supervised_learning/0x0F-word_embeddings/2-word2vec.py
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-""" 2. Train Word2Vec """
+"""NLP MODULE"""
 from gensim.models import Word2Vec
 
 
-def word2vec_model(
+def word2vec_model(sentences, size=100, min_count=5, window=5, negative=5,
+                   cbow=True, iterations=5, seed=0, workers=1):
+    """creates and trains a gensim word2vec model"""
+    model = Word2Vec(
+        sentences=sentences,
+        size=size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=not cbow,
+        iter=iterations,
+        seed=seed,
+        workers=workers)
+    model.train(
         sentences,
-        size=100,
-        min_count=5,
-        window=5,
-        negative=5,
-        cbow=True,
-        iterations=5,
-        seed=0,
-        workers=1):
-    """function that creates and trains a gensim word2vec model"""
-    model = Word2Vec(sentences=sentences,
-                     window=window, seed=seed, negative=negative,
-                     sg=int(not cbow), vector_size=size, min_count=min_count,
-                     workers=workers, epochs=iterations)
-    token_count = sum([len(sentence) for sentence in sentences])
-    model.train(sentences, total_examples=token_count, epochs=model.epochs)
+        total_examples=model.corpus_count,
+        epochs=model.epochs)
     return model
