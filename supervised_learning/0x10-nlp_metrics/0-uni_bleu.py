62f6608f73a0d159134aaedcc58a6bc686939958
jlassi1 <khawlajlassi1990@gmail.com>
2021-05-14 22:13:04 +0100

    https://ariepratama.github.io/Introduction-to-BLEU-in-python/#content

---
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" 0. Unigram BLEU score """
+import numpy as np
+
+
+def uni_bleu(references, sentence):
+    """function  that calculates the unigram BLEU score for a sentence"""
+    c = len(sentence)
+    bp = brevity_penalty(sentence, references)
+    words = count_clip_ngram(sentence, references)
+    p = sum(words.values()) / c
+    return bp * p
+
+
+def brevity_penalty(candidate, references):
+    """
+    Brevity Penalty
+    BP={1 if c>r or exp(1−r/c)if c≤r
+    c: length of candidate translation
+
+    r: effective reference length
+    """
+    c = len(candidate)
+    r = np.argmin(np.abs(len(r) - c) for r in references)
+    r = len(references[r])
+
+    if c > r:
+        return 1
+    else:
+        return np.exp(1 - r / c)
+
+
+def count_clip_ngram(sentence, references):
+    """
+    Countclip=min(Count,Max_Ref_Count)
+    """
+    words = dict()
+    for word in sentence:
+        for ref in references:
+            if word in words:
+                if words[word] < ref.count(word):
+                    words.update({word: ref.count(word)})
+            else:
+                words.update({word: ref.count(word)})
+    return words
