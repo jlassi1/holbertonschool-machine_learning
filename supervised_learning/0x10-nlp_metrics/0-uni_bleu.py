616d07a0034d8796d71f6c259093de7ecd6c071f
jlassi1 <khawlajlassi1990@gmail.com>
2021-05-16 18:10:28 +0100

    0-uni_bleu.py

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -1,25 +1,44 @@
 #!/usr/bin/env python3
-"""NLP METRICS"""
+""" 0. Unigram BLEU score """
 import numpy as np
-from collections import Counter
 
 
 def uni_bleu(references, sentence):
-    """calculates the unigram BLEU score for a sentence"""
-    unigrams = len(sentence)
-    token = np.array([len(r) for r in references])
-    idx = np.argmin(np.abs(token - unigrams))
-    r = len(references[idx])
-    bp = 1
-    if r > unigrams:
-        bp = np.exp(1 - r / unigrams)
-    words = {}
-    for i in sentence:
+    """function  that calculates the unigram BLEU score for a sentence"""
+    c = len(sentence)
+    bp = brevity_penalty(sentence, references)
+    words = count_clip_ngram(sentence, references)
+    p = sum(words.values())
+    return bp * p / c
+
+
+def brevity_penalty(candidate, references):
+    """
+    Brevity Penalty
+    BP={1 if c>r or exp(1−r/c)if c≤r
+    c: length of candidate translation
+    r: effective reference length
+    """
+    c = len(candidate)
+    r = np.array([len(r) for r in references])
+    r = np.argmin(np.abs(r - c))
+    r = len(references[r])
+    if c > r:
+        return 1
+    else:
+        return np.exp(1 - float(r) / c)
+
+
+def count_clip_ngram(sentence, references):
+    """
+    Countclip=min(Count,Max_Ref_Count)
+    """
+    words = dict()
+    for word in sentence:
         for ref in references:
-            if i in words:
-                if words[i] < ref.count(i):
-                    words.update({i: ref.count(i)})
+            if word in words:
+                if words[word] < ref.count(word):
+                    words.update({word: ref.count(word)})
             else:
-                words.update({i: ref.count(i)})
-    p = sum(words.values()) / unigrams
-    return bp * p
+                words.update({word: ref.count(word)})
+    return words
