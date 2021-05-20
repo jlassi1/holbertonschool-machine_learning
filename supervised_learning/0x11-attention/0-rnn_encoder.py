64fd0b48462b5cf3bdfa5df99b70ae4fc267613a
jlassi1 <khawlajlassi1990@gmail.com>
2021-05-20 22:06:22 +0100

    0-rnn_encoder.py

---
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""0. RNN Encoder """
+import tensorflow as tf
+from tensorflow import keras as K
+
+
+class RNNEncoder(tf.keras.layers.Layer):
+    """a class from tensorflow.keras that encode for machine translation"""
+    def __init__(self, vocab, embedding, units, batch):
+        """ initialization """
+        super(RNNEncoder, self).__init__()
+        self.batch = batch
+        self.units = units
+        self.embedding = K.layers.Embedding(vocab, embedding)
+        self.gru = K.layers.GRU(units, kernel_initializer="glorot_uniform",
+                                return_sequences=True, return_state=True)
+
+    def initialize_hidden_state(self):
+        """Initializes the hidden states for
+        the RNN cell to a tensor of zeros"""
+        return tf.zeros((self.batch, self.units))
+
+    def call(self, x, initial):
+        """call funtion """
+        x = self.embedding(x)
+        return self.gru(x, initial)
