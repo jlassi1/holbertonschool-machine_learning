7487d4004c8a649ea7a55477a670812d2d9c543b
jlassi1 <khawlajlassi1990@gmail.com>
2021-06-01 16:25:05 +0100

    trainnig done

--- supervised_learning/0x12-transformer_apps/5-train.py
+++ supervised_learning/0x12-transformer_apps/5-train.py
@@ -1,22 +1,10 @@
 #!/usr/bin/env python3
-""" Train transformer """
+""" Transformer Trainning"""
 import tensorflow as tf
 import numpy as np
 Dataset = __import__('3-dataset').Dataset
 create_masks = __import__('4-create_masks').create_masks
 Transformer = __import__('5-transformer').Transformer
-
-
-def loss_function(real, pred):
-    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True, reduction='none')
-    mask = tf.math.logical_not(tf.math.equal(real, 0))
-    loss_ = loss_object(real, pred)
-
-    mask = tf.cast(mask, dtype=loss_.dtype)
-    loss_ *= mask
-
-    return tf.reduce_sum(loss_) / tf.reduce_sum(mask)
 
 
 class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
@@ -35,17 +23,24 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
 
 
-train_step_signature = [
-    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
-    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
-]
+def loss_function(real, pred):
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_object(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+
+    return tf.reduce_sum(loss_) / tf.reduce_sum(mask)
 
 
-@tf.function(input_signature=train_step_signature)
-def train_step(inp, tar):
-    train_loss = tf.keras.metrics.Mean(name='train_loss')
-    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
-        name='train_accuracy')
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+    name='train_accuracy')
+
+
+def train_step(inp, tar, transformer, optimizer):
     tar_inp = tar[:, :-1]
     tar_real = tar[:, 1:]
 
@@ -53,16 +48,17 @@ def train_step(inp, tar):
         inp, tar_inp)
 
     with tf.GradientTape() as tape:
-        predictions, _ = transformer(inp, tar_inp,
-                                     True,
-                                     enc_padding_mask,
-                                     combined_mask,
-                                     dec_padding_mask)
+        predictions = transformer(
+            inputs=inp,
+            target=tar_inp,
+            training=True,
+            encoder_mask=enc_padding_mask,
+            look_ahead_mask=combined_mask,
+            decoder_mask=dec_padding_mask)
         loss = loss_function(tar_real, predictions)
 
     gradients = tape.gradient(loss, transformer.trainable_variables)
     optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
-
     train_loss(loss)
     train_accuracy(tar_real, predictions)
 
@@ -70,30 +66,39 @@ def train_step(inp, tar):
 def train_transformer(N, dm, h, hidden, max_len, batch_size, epochs):
     """creates and trains a transformer model for
     machine translation of Portuguese to English"""
-    learning_rate = CustomSchedule(dm)
 
+    learning_rate = CustomSchedule(dm)
     optimizer = tf.keras.optimizers.Adam(
         learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
     data = Dataset(batch_size, max_len)
-    pt, en = data.data_train
-    encoder_mask, combined_mask, decoder_mask = create_masks(pt, en)
+    vocab_size_pt = data.tokenizer_pt.vocab_size + 2
+    vocab_size_en = data.tokenizer_en.vocab_size + 2
     transformer = Transformer(
-        N, dm, h, hidden, pt.vocab_size + 2,
-        en.vocab_size + 2, pt.vocab_size + 2, en.vocab_size + 2)
+        N,
+        dm,
+        h,
+        hidden,
+        vocab_size_pt,
+        vocab_size_en,
+        max_len,
+        max_len)
+
     for epoch in range(epochs):
-        start = time.time()
 
         train_loss.reset_states()
         train_accuracy.reset_states()
 
-        # inp -> portuguese, tar -> english
-        for (batch, (inp, tar)) in enumerate(train_dataset):
-            train_step(inp, tar)
+        for (batch, (inp, tar)) in enumerate(data.data_train):
+            train_step(inp, tar, transformer, optimizer)
 
             if batch % 50 == 0:
-                print ('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
-                    epoch + 1, batch, train_loss.result(),
-                    train_accuracy.result()))
+                print (
+                    'Epoch {} Batch {} Loss {} Accuracy {}'.format(
+                        epoch + 1,
+                        batch,
+                        train_loss.result(),
+                        train_accuracy.result()))
 
-        print ('Epoch {} Loss {:.4f} Accuracy {:.4f}'.format(
-            epoch + 1, train_loss.result(), train_accuracy.result()))
+        print ('Epoch {} Loss {} Accuracy {}'.format(epoch + 1,
+                                                     train_loss.result(),
+                                                     train_accuracy.result()))
