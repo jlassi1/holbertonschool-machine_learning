05d3f7a53656186f1c86e350deebf33166a2b300
jlassi1 <khawlajlassi1990@gmail.com>
2021-08-02 12:19:18 +0100

    7-max_state.sql

---
+++ pipeline/0x02-databases/7-max_state.sql
@@ -0,0 +1,6 @@
+-- Temperatures #2
+-- script that displays the max temperature of each state (ordered by State name).
+SELECT state, max(value) as max_temp 
+FROM temperatures
+GROUP BY state
+ORDER BY state;
