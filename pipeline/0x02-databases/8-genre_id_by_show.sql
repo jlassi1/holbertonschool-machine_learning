2be89c21626248f33243a9b3ea546831adf93d2f
jlassi1 <khawlajlassi1990@gmail.com>
2021-08-02 12:19:34 +0100

    8-genre_id_by_show.sql

---
+++ pipeline/0x02-databases/8-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- Genre ID by show
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY title, genre_id ASC;
