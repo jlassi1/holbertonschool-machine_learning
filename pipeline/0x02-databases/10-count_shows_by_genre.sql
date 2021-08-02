caaebbeb08785cb699a2cf05f472272e63c02d13
jlassi1 <khawlajlassi1990@gmail.com>
2021-08-02 12:34:20 +0100

    10-count_shows_by_genre.sql

--- pipeline/0x02-databases/10-count_shows_by_genre.sql
+++ pipeline/0x02-databases/10-count_shows_by_genre.sql
@@ -1,8 +1,10 @@
 -- Number of shows by genre 
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_show_genres
 INNER JOIN tv_genres
 WHERE tv_genres.id = tv_show_genres.show_id
-GROUP BY tv_genres.name 
+GROUP BY tv_genres.name
+HAVING COUNT(*) != 0
 ORDER BY  number_of_shows DESC;
