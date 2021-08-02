d6cb6a0d41c43392af7a7460567d6d7243d04bae
jlassi1 <khawlajlassi1990@gmail.com>
2021-08-02 12:55:32 +0100

    12-rating_genres.sql

--- pipeline/0x02-databases/12-rating_genres.sql
+++ pipeline/0x02-databases/12-rating_genres.sql
@@ -1,9 +1,9 @@
 --  Best genre
 -- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 
-SELECT name AS name, SUM(rate) AS rating 
-FROM tv_genres
+SELECT tv_genres.name AS name, SUM(rate) AS rating 
+FROM tv_show_genres, tv_genres
 INNER JOIN tv_show_ratings
-WHERE tv_genres.id = tv_show_ratings.show_id
-GROUP BY name
+WHERE tv_show_genres.show_id = tv_show_ratings.show_id AND tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
 ORDER BY  rating  DESC;
