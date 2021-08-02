b2c36b5e22405f73c4f005cb5d8b62d6d1682034
jlassi1 <khawlajlassi1990@gmail.com>
2021-08-02 14:44:06 +0100

    20-average_score.sql

---
+++ pipeline/0x02-databases/20-average_score.sql
@@ -0,0 +1,14 @@
+-- Average score 
+-- script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
+CREATE Procedure ComputeAverageScoreForUser(IN user_id INT)
+    BEGIN
+        DECLARE avg INT DEFAULT 0;
+    
+        SET avg = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
+        UPDATE users SET users.average_score = avg WHERE users.id = user_id;
+
+    END//
+DELIMITER ;
