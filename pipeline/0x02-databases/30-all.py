65a68bf7b873904ce91fdae3ba64592d493750c4
jlassi1 <khawlajlassi1990@gmail.com>
2021-08-03 15:09:31 +0100

    30-all.py

---
+++ pipeline/0x02-databases/30-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""List all documents in Python """
+
+
+def list_all(mongo_collection):
+    """Python function that lists all documents in a collection"""
+    return mongo_collection.find()
