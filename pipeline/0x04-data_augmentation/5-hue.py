42d1184a6babfab2fd88d33772ceb1ad62b3823a
jlassi1 <khawlajlassi1990@gmail.com>
2021-08-10 14:59:05 +0100

    changes the hue of img*

---
+++ pipeline/0x04-data_augmentation/5-hue.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" 5. Hue """
+
+import tensorflow as tf
+
+
+def change_hue(image, delta):
+    """function that changes the hue of an image"""
+    return tf.image.adjust_hue(image, delta)
