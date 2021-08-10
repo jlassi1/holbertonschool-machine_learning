90dc25174eebb94284d492cdb211451d25185716
jlassi1 <khawlajlassi1990@gmail.com>
2021-08-10 14:58:00 +0100

    randomly shears an img

---
+++ pipeline/0x04-data_augmentation/3-shear.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""3. Shear"""
+
+import tensorflow as tf
+
+
+def shear_image(image, intensity):
+    """function that randomly shears an image"""
+    return tf.keras.preprocessing.image.random_shear(
+        image, intensity)
